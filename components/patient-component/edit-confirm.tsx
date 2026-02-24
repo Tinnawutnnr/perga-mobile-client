8f2537eb8fb21ad93a5df6a3ea302ed564f90940
Parunchai Kochseni <parunchai.koch@gmail.com>
2026-02-24 13:26:53 +0700

    Update edit-confirm.tsx

--- components/patient-component/edit-confirm.tsx
+++ components/patient-component/edit-confirm.tsx
@@ -10,7 +10,7 @@ import {
     View,
 } from "react-native";
 import { Patient } from "@/data/mockPatient";
-import PrimaryInput from "../primary-input";
+import PrimaryInput from "@/primary-input";
 
 type Props = {
   visible: boolean;
