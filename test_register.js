01e201c53a5749c00e35f9e71b86c3dd6ed365d1
Tinnawutnnr <Tinnawut.nnr@gmail.com>
2026-04-21 15:58:20 +0700

    chore: remove deprecated registration test script

--- test_register.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-axios.post('http://localhost:8000/api/v1/auth/register', {
-  email: 'test@example.com',
-  username: 'testuser123',
-  password: 'Password123!',
-  role: 'caregiver'
-}).catch(err => console.log(JSON.stringify(err.response.data, null, 2)));
